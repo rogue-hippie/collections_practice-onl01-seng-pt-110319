87b0f3800ba44c7d6fb6d2fff7f5cbe8e62ef4c0
Whitney Hodgkins <hodgkins.whitney@gmail.com>
2019-11-24 03:15:55 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+  a <=> b
+end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a 
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  kesha = []
+  
+  array.each do |name|
+    name[2] = "$"
+    kesha << name
+  end
+  kesha
+end
+
+def find_a(array)
+  array.select{|i| i.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject(0){|sum,x| sum + x }
+end
+
+def add_s(array)
+  array.each_with_index.collect{|element, index| 
+      if index != 1
+     element + "s"
+  elsif index == 1
+     element
+  end
+     }
+end 
+
+
+  
